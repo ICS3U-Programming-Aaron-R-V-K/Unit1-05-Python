491d7f3a0754ec9e49d1289540b4cb0d8060ea38
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-02-18 17:35:02 +0000

    area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created By: Aaron Rivelino
+# Date: Feb 15, 2025
+# The code does simple math calculations
+
+print(
+    "area of a rectangle with 345cm in length and 299cm width = {}cm".format(345 * 299)
+)
